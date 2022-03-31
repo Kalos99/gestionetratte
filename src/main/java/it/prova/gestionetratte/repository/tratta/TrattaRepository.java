f7c25ad4935db1717d895490ef73cb2221036834
Kalos99 <calogerocorsello1999@gmail.com>
2022-03-31 11:40:32 +0200

    Setup package repository

---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/TrattaRepository.java
@@ -0,0 +1,21 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public interface TrattaRepository extends PagingAndSortingRepository<Tratta, Long>, JpaSpecificationExecutor<Tratta>{
+	
+	@Query("from Tratta t join fetch t.airbus where t.id = ?1")
+	Tratta findSingleTrattaEager(Long id);
+	
+	List<Tratta> findByCodiceAndDescrizione(String codice, String descrizione);
+	
+	@Query("select t trom Tratta t join fetch t.airbus")
+	List<Tratta> findAllTratteEager();
+
+}
